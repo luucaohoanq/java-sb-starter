ad29ea01f373379932f03a7cb023e95fab4e2dc6
lcaohoanq <hoangclw@gmail.com>
2025-09-25 16:05:38 +0700

    feat: implement CRLF log filtering and enhance application startup logging

--- src/main/java/com/orchid/orchidbe/App.java
+++ src/main/java/com/orchid/orchidbe/App.java
@@ -7,10 +7,18 @@
  */
 package com.orchid.orchidbe;
 
+import com.orchid.orchidbe.configs.CRLFLogConverter;
 import io.github.lcaohoanq.annotations.BrowserLauncher;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Optional;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
@@ -18,11 +26,65 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EntityScan(basePackages = "com.orchid.orchidbe.domain")
 @BrowserLauncher(
     value = "http://localhost:8080/swagger-ui.html",
-    healthCheckEndpoint = "http://localhost:8080/actuator/health",
-    excludeProfiles = {"h2", "test"})
+    healthCheckEndpoint = "http://localhost:8080/actuator/health"
+    //    excludeProfiles = {"h2", "test"}
+    )
 public class App {
 
+  private static final Logger LOG = LoggerFactory.getLogger(App.class);
+
+  private final Environment env;
+
+  public App(Environment env) {
+    this.env = env;
+  }
+
+  /**
+   * Main method, used to run the application.
+   *
+   * @param args the command line arguments.
+   */
   public static void main(String[] args) {
-    SpringApplication.run(App.class, args);
+
+    var env = SpringApplication.run(App.class, args);
+    logApplicationStartup(env.getEnvironment());
+  }
+
+  private static void logApplicationStartup(Environment env) {
+    String protocol =
+        Optional.ofNullable(env.getProperty("server.ssl.key-store"))
+            .map(key -> "https")
+            .orElse("http");
+    String applicationName = env.getProperty("spring.application.name");
+    String serverPort = env.getProperty("server.port");
+    String contextPath =
+        Optional.ofNullable(env.getProperty("server.servlet.context-path"))
+            .filter(StringUtils::isNotBlank)
+            .orElse("/");
+    String hostAddress = "localhost";
+    try {
+      hostAddress = InetAddress.getLocalHost().getHostAddress();
+    } catch (UnknownHostException e) {
+      LOG.warn("The host name could not be determined, using `localhost` as fallback");
+    }
+    LOG.info(
+        CRLFLogConverter.CRLF_SAFE_MARKER,
+        """
+
+        ----------------------------------------------------------
+        \tApplication '{}' is running! Access URLs:
+        \tLocal: \t\t{}://localhost:{}{}
+        \tExternal: \t{}://{}:{}{}
+        \tProfile(s): \t{}
+        ----------------------------------------------------------""",
+        applicationName,
+        protocol,
+        serverPort,
+        contextPath,
+        protocol,
+        hostAddress,
+        serverPort,
+        contextPath,
+        env.getActiveProfiles().length == 0 ? env.getDefaultProfiles() : env.getActiveProfiles());
   }
 }
